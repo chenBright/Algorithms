4c9e04be154c624ba350740e9199f1e742706f6a
chenBright <1021774709@qq.com>
2018-02-28 17:08:27 +0800

    Feature_习题1.1.6

--- src/com/chenBright/algorithms/Main.java
+++ src/com/chenBright/algorithms/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Ex1_1_1.main();
         Ex1_1_2.main();
 //        Ex1_1_3.main();
-        Ex1_1_5.main();
+//        Ex1_1_5.main();
+        Ex1_1_6.main();
     }
 }
