a4882b98bfb1b97cfe0fc4d7449ee5e64c9c0d86
chenBright <1021774709@qq.com>
2018-03-01 09:31:46 +0800

    Feature_习题1.1.13

--- src/com/chenBright/algorithms/Exercise.java
+++ src/com/chenBright/algorithms/Exercise.java
@@ -61,6 +61,9 @@ public class Exercise {
             case 9:
                 Ex1_1_9.main();
                 break;
+            case 13:
+                Ex1_1_13.main();
+                break;
             default:
                 StdOut.printf("没有第 %d 题的答案！", question);
         }
