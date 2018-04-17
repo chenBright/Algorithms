22f5182d8d3a8b553617386b6daaf360e904220e
chenBright <1021774709@qq.com>
2018-04-17 19:09:17 +0800

    Feature_1.4习题1.4.20 (#37)

--- src/com/chenBright/algorithms/chapter1_1/BinarySearch.java
+++ src/com/chenBright/algorithms/chapter1_1/BinarySearch.java
@@ -8,15 +8,15 @@ public class BinarySearch {
         return rank(key, a, 0, a.length - 1);
     }
 
-    public static int rank(int key, int[] a, int lo, int hi) {
-        if (lo > hi) {
+    public static int rank(int key, int[] a, int low, int high) {
+        if (low > high) {
             return -1;
         }
-        int mid = lo + (hi - lo) / 2;
+        int mid = low + (high - low) / 2;
         if (key < a[mid]) {
-            return rank(key, a, lo, mid - 1);
+            return rank(key, a, low, mid - 1);
         } else if (key > a[mid]) {
-            return rank(key, a, mid + 1, hi);
+            return rank(key, a, mid + 1, high);
         } else {
             return mid;
         }
