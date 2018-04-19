30302703b05da6a8451d11c9095e5a13d9838745
chenBright <1021774709@qq.com>
2018-04-19 23:59:39 +0800

    Feature_1.5union-find (#41)
    * Feature_1.5union-find
    
    * Feature_1.5union-find测试

--- src/com/chenBright/algorithms/Main.java
+++ src/com/chenBright/algorithms/Main.java
@@ -7,13 +7,14 @@ import com.chenBright.algorithms.chapter1_1.*;
 import com.chenBright.algorithms.chapter1_2.*;
 import com.chenBright.algorithms.chapter1_3.*;
 import com.chenBright.algorithms.chapter1_4.*;
+import com.chenBright.algorithms.chapter1_5.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         StdOut.println("算法4");
-        Player.main();
+        UF.main();
 //        Ex1_4_19 ex = new Ex1_4_19();
 //        ex.main();
     }
