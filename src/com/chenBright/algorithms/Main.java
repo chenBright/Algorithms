c7a228363c0249d2285a8e00dd0f25862519b90c
chenBright <1021774709@qq.com>
2018-04-22 18:42:58 +0800

    Feature_2.1插入排序

--- src/com/chenBright/algorithms/Main.java
+++ src/com/chenBright/algorithms/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
 
         StdOut.println("算法4");
-        Selection.main();
+        Insertion.main();
 //        Ex1_4_19 ex = new Ex1_4_19();
 //        ex.main();
     }
