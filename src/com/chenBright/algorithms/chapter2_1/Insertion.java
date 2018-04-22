b6b1d050b963b05a1e6a54185a066c8ce276e35b
chenBright <1021774709@qq.com>
2018-04-22 18:56:03 +0800

    Feature_2.1习题2.1.18

--- src/com/chenBright/algorithms/chapter2_1/Insertion.java
+++ src/com/chenBright/algorithms/chapter2_1/Insertion.java
@@ -1,7 +1,10 @@
 package com.chenBright.algorithms.chapter2_1;
 
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+
+import java.awt.*;
 
 /**
  * Created by chenbright on 2018/4/22.
@@ -42,10 +45,53 @@ public class Insertion {
         return true;
     }
 
+    /**
+     * Exercise 2.1.18
+     *
+     * @param a
+     */
+    public static void drawSort(Comparable[] a) {
+        int N = a.length;
+        StdDraw.setYscale(N + 3, 0);
+        StdDraw.setXscale(0, N + 3);
+        StdDraw.setPenColor(Color.BLACK);
+        StdDraw.text(3 + N / 2, 0.4, "a[ ]");
+        StdDraw.text(1, 1, "i");
+        StdDraw.text(2, 1, "j");
+        for (int i = 0; i < N; i++) {
+            StdDraw.text(3 + i, 1, i + "");
+            StdDraw.text(3 + i, 2, a[i].toString());
+        }
+        StdDraw.line(0, 1.5, N + 3, 1.5);
+        for (int i = 1; i < N; i++) {
+            StdDraw.text(1, 2 + i, i + "");
+            int red = i;
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+                red = j - 1;
+                StdDraw.text(3 + j, 2 + i, a[j].toString());
+            }
+            StdDraw.text(2, 2 + i, red + "");
+            StdDraw.setPenColor(Color.RED);
+            StdDraw.text(3 + red, 2 + i, a[red].toString());
+            StdDraw.setPenColor(Color.GRAY);
+            for (int k = 0; k < N; k++) {
+                if (k < red || k > i) {
+                    StdDraw.text(3 + k, 2 + i, a[k].toString());
+                }
+            }
+            StdDraw.setPenColor(Color.BLACK);
+        }
+        for (int i = 0; i < N; i++) {
+            StdDraw.text(3 + i, N + 2, a[i].toString());
+        }
+    }
+
     public static void main() {
         Integer[] a = {9, 4, 2, 8, 7, 1};
 //        String[] a = StdIn.readAllStrings();
-        sort(a);
+//        sort(a);
+        drawSort(a);
         assert isSorted(a);
         show(a);
     }
--- src/com/chenBright/algorithms/chapter2_1/Selection.java
+++ src/com/chenBright/algorithms/chapter2_1/Selection.java
@@ -1,7 +1,10 @@
 package com.chenBright.algorithms.chapter2_1;
 
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+
+import java.awt.*;
 
 /**
  * Created by chenbright on 2018/4/22.
@@ -46,10 +49,57 @@ public class Selection {
         return true;
     }
 
+    /**
+     * Exercise 2.1.18
+     *
+     * @param a
+     */
+    public static void drawSort(Comparable[] a) {
+        int N = a.length;
+        StdDraw.setYscale(N + 4, 0);
+        StdDraw.setXscale(0, N + 3);
+        StdDraw.setPenColor(Color.BLACK);
+        StdDraw.text(3 + N / 2, 0.4, "a[ ]");
+        StdDraw.textRight(1, 1, "i");
+        StdDraw.textRight(2, 1, "min");
+        for (int i = 0; i < N; i++) {
+            StdDraw.text(3 + i, 1, i + "");
+            StdDraw.text(3 + i, 2, a[i].toString());
+        }
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            StdDraw.textRight(1, 3 + i, i + "");
+            StdDraw.textRight(2, 3 + i, min + "");
+            StdDraw.setPenColor(Color.GRAY);
+            for (int j = 0; j < N; j++) {
+                if (j == i) {
+                    StdDraw.setPenColor(Color.BLACK);
+                }
+                if (j == min) {
+                    StdDraw.setPenColor(Color.RED);
+                }
+                StdDraw.text(3 + j, 3 + i, a[j].toString());
+                if (j == min) {
+                    StdDraw.setPenColor(Color.BLACK);
+                }
+            }
+            exch(a, i, min);
+        }
+        for (int i = 0; i < N; i++) {
+            StdDraw.text(3 + i, N + 3, a[i].toString());
+        }
+    }
+
     public static void main() {
 //        Integer[] a = {9, 4, 2, 8, 7, 1};
         String[] a = StdIn.readAllStrings();
-        sort(a);
+//        sort(a);
+        drawSort(a);
         assert isSorted(a);
         show(a);
     }
