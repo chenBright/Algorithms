0b94e971020a22f7f02674ff42a4ef25c78f6641
chenBright <1021774709@qq.com>
2018-04-24 18:16:43 +0800

    Feature_2.1习题2.1.1

---
+++ src/com/chenBright/algorithms/chapter2_1/BaseSort.java
@@ -0,0 +1,35 @@
+package com.chenBright.algorithms.chapter2_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by chenbright on 2018/4/24.
+ * 排序模板
+ */
+public class BaseSort {
+    protected static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    protected static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    protected static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    protected static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
