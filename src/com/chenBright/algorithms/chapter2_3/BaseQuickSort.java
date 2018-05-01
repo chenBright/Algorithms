d7a927b146f77808b84fda1b71c48d14bbc58915
chenBright <1021774709@qq.com>
2018-05-01 17:55:11 +0800

    Feature_2.3 快排 (#58)
    * Feature_2.3 快排
    
    * Feature_2.3 三向切分的快排

---
+++ src/com/chenBright/algorithms/chapter2_3/BaseQuickSort.java
@@ -0,0 +1,34 @@
+package com.chenBright.algorithms.chapter2_3;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by chenbright on 2018/5/1.
+ */
+public class BaseQuickSort {
+    protected static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    protected static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    protected static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    protected static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
