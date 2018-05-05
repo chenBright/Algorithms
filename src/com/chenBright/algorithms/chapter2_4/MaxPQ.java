218f7b8e17ed3bdcab8e52be0bb95a194e6d3a25
chenBright <1021774709@qq.com>
2018-05-05 21:36:51 +0800

    Feature_2.4 优先队列二叉堆实现

--- src/com/chenBright/algorithms/chapter2_4/MaxPQ.java
+++ src/com/chenBright/algorithms/chapter2_4/MaxPQ.java
@@ -6,74 +6,70 @@ package com.chenBright.algorithms.chapter2_4;
 public class MaxPQ<Key extends Comparable<Key>> {
     private Key[] pq;
     private int N = 0;
-    private Key min;
 
-    /**
-     * 构造函数
-     * @param max 队列最大容量
-     */
-    public MaxPQ(int max) {
-        pq = (Key[]) new Comparable[max + 1];
+    public MaxPQ(int MaxN) {
+        pq = (Key[]) new Comparable[MaxN + 1];
     }
 
-    /**
-     * 向优先队列中插入一个元素
-     * @param v 元素
-     */
-    public void insert(Key v) {
-        pq[N++] = v;
-        for (int i = N - 1; i > 0 && less(i, i - 1); i++) {
-            exch(i, i - 1);
-        }
-    }
-
-    /**
-     * 优先队列是否为空
-     * @return true / false
-     */
     public boolean isEmpty() {
         return N == 0;
     }
 
-    /**
-     * 优先队列中元素个数
-     * @return
-     */
     public int size() {
         return N;
     }
 
-    /**
-     * 返回最大元素
-     * @return
-     */
+    public void insert(Key v) {
+        pq[++N] = v;
+        swim(N);
+    }
+
     public Key max() {
-        return pq[N - 1];
+        return pq[1];
     }
 
-    /**
-     * 删除并返回最大元素
-     * @return
-     */
     public Key delMax() {
-        return pq[--N];
+        Key max = pq[1];
+        exch(1, N--);
+        pq[N + 1] = null; // 防止对象游离
+        sink(1);
+        return max;
     }
 
     /**
-     * 优先队列中索引号为i的元素是否比索引号为j的元素小
-     * @param i
-     * @param j
-     * @return true / false
+     * 由下至上的堆有序化（上浮）
+     * @param k 上浮第k个结点
      */
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exch(k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    /**
+     * 由上至下的堆有序化（下沉）
+     * @param k 下沉第k个结点
+     */
+    private void sink(int k) {
+        while (k * 2 >= N) {
+            int j = k * 2;
+            // 找到最大的子结点
+            if (j < N && less(j, j + 1)) {
+                j++;
+            }
+            if (!less(k, j)) {
+                break;
+            }
+            exch(k, j);
+            k = j;
+        }
+    }
+
     private boolean less(int i, int j) {
         return pq[i].compareTo(pq[j]) < 0;
     }
 
-    /**
-     * 将索引号为i的元素和索引号为j的元素交换位置
-     * @param i
-     * @param j
-     */
     private void exch(int i, int j) {
         Key temp = pq[i];
         pq[i] = pq[j];
