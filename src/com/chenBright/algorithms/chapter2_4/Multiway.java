89c569f215426cf302065d7b9ab5c80d3dfed71f
chenBright <1021774709@qq.com>
2018-05-06 15:53:03 +0800

    Feature_2.4 索引优先队列用例 (#65)

---
+++ src/com/chenBright/algorithms/chapter2_4/Multiway.java
@@ -0,0 +1,37 @@
+package com.chenBright.algorithms.chapter2_4;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.IndexMinPQ;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by chenbright on 2018/5/6.
+ */
+public class Multiway {
+    public static void merge(In[] streams) {
+        int N = streams.length;
+        IndexMinPQ<String> pq = new IndexMinPQ<String>(N);
+        for (int i = 0; i < N; i++) {
+            if (!streams[i].isEmpty()) {
+                pq.insert(i, streams[i].readString());
+            }
+        }
+        while (!pq.isEmpty()) {
+            StdOut.println(pq.minKey());
+            int i = pq.delMin();
+            if (!streams[i].isEmpty()) {
+                pq.insert(i, streams[i].readString());
+            }
+        }
+    }
+
+    public static void main() {
+        String[] args = {"data/m1.txt", "data/m2.txt", "data/m3.txt"};
+        int N = args.length;
+        In[] streams = new In[N];
+        for (int i = 0; i < N; i++) {
+            streams[i] = new In(args[i]);
+        }
+        merge(streams);
+    }
+}
