5925546eef14a5d1cdd862941f5839766755c5ac
chenBright <1021774709@qq.com>
2018-05-14 16:57:47 +0800

    Feature_3.2 习题3.2.32

--- src/com/chenBright/algorithms/chapter3_2/BST.java
+++ src/com/chenBright/algorithms/chapter3_2/BST.java
@@ -296,4 +296,21 @@ public class BST<Key extends Comparable<Key>, Value> {
             keys(node.right, queue, low, high);
         }
     }
+    // 习题3.2.32
+    private boolean isBST() {
+        return isBST(root, null, null);
+    }
+
+    private boolean isBST(Node x, Key min, Key max) {
+        if (x == null) {
+            return true;
+        }
+        if (min != null && x.key.compareTo(min) <= 0) {
+            return false;
+        }
+        if (max != null && x.key.compareTo(max) >= 0) {
+            return false;
+        }
+        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+    }
 }
