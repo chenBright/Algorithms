28285454a32ed3f36655d9480f25adc5ddc32635
chenBright <1021774709@qq.com>
2018-05-14 16:27:54 +0800

    Feature_3.2 非递归BST

--- src/com/chenBright/algorithms/chapter3_2/NonrecursiveBS.java
+++ src/com/chenBright/algorithms/chapter3_2/NonrecursiveBS.java
@@ -1,8 +1,10 @@
 package com.chenBright.algorithms.chapter3_2;
 
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdIn;
 
-public class Ex3_2_13<Key extends Comparable<Key>, Value> {
+public class NonrecursiveBS<Key extends Comparable<Key>, Value> {
     private Node root;
 
     private class Node {
@@ -56,10 +58,54 @@ public class Ex3_2_13<Key extends Comparable<Key>, Value> {
 
         int cmp = key.compareTo(parent.key);
         if (cmp < 0) {
-            node.left = new Node(key, val);
+            parent.left = new Node(key, val);
         }
         else if (cmp > 0){
-            node.right = new Node(key, val);
+            parent.right = new Node(key, val);
         }
+    }
+
+    public Key min() {
+        if (root == null) {
+            return null;
+        }
+        Node node = root, parent = null;
+        while (node != null) {
+            parent = node;
+            node = node.left;
+        }
+        // node为null，则parent为最小结点
+        return parent.key;
+    }
+
+    public Key max() {
+        if (root == null) {
+            return null;
+        }
+        Node node = root, parent = null;
+        while (node != null) {
+            parent = node;
+            node = node.right;
+        }
+        return parent.key;
+    }
+
+    // 非递归的中序遍历
+    public Iterable<Key> keys() {
+        Stack<Node> stack = new Stack<Node>();
+        Queue<Key> queue = new Queue<Key>();
+        Node node = root;
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            else {
+                node = stack.pop();
+                queue.enqueue(node.key);
+                node = node.right;
+            }
+        }
+        return queue;
     }
 }
