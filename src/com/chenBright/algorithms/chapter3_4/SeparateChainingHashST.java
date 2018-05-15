fba377b14daa1dd37c1106d9b0d95e4b55b054f4
chenBright <1021774709@qq.com>
2018-05-15 16:06:49 +0800

    Feature_3.4 SeparateChainingHashST

--- src/com/chenBright/algorithms/chapter3_4/SeparateChainingHashST.java
+++ src/com/chenBright/algorithms/chapter3_4/SeparateChainingHashST.java
@@ -22,6 +22,18 @@ public class SeparateChainingHashST<Key, Value> {
         }
     }
 
+    private void resize(int chains) {
+        SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST<Key, Value>(chains);
+        for (int i = 0; i < M; i++) {
+            for (Key key : st[i].keys()) {
+                temp.put(key, st[i].get(key));
+            }
+        }
+        this.M  = temp.M;
+        this.N  = temp.N;
+        this.st = temp.st;
+    }
+
     private int hash(Key key) {
         return (key.hashCode() & 0x7fffffff) % M;
     }
@@ -31,7 +43,19 @@ public class SeparateChainingHashST<Key, Value> {
     }
 
     public void put(Key key, Value val) {
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        if (N >= 10*M) {
+            resize(2*M);
+        }
         st[hash(key)].put(key, val);
+    }
+
+    public void delete(Key key) {
+        int i = hash(key);
+        st[i].delete(key);
     }
 
     public Iterable<Key> keys() {
