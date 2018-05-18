4a6da9459b2c00a0b32654a516ddd06cff72a959
chenBright <1021774709@qq.com>
2018-05-18 17:05:13 +0800

    Feature_4.1 Graph (#98)

---
+++ src/com/chenBright/algorithms/chapter4_1/Graph.java
@@ -0,0 +1,60 @@
+package com.chenBright.algorithms.chapter4_1;
+
+import com.chenBright.algorithms.chapter1_3.Bag;
+import edu.princeton.cs.algs4.In;
+
+public class Graph {
+    private final int V; // 顶点数目
+    private int E; // 边的数目
+    private Bag<Integer>[] adj; // 邻接表
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new Bag<Integer>();
+        }
+    }
+
+    public Graph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj(v);
+    }
+
+    @Override
+    public String toString() {
+        String s = V + " vertices, " + E + " edges\n";
+        for (int v = 0; v < V; v++) {
+            s += v + ": ";
+            for (int w : this.adj(v)) {
+                s += w + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+}
