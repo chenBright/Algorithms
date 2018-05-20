75466eb6151e695194fdb7698729812c0e3621ec
chenBright <1021774709@qq.com>
2018-05-20 16:35:04 +0800

    Feature_4.1 BreadthFirstPaths (#102)

---
+++ src/com/chenBright/algorithms/chapter4_1/BreadthFirstPaths.java
@@ -0,0 +1,49 @@
+package com.chenBright.algorithms.chapter4_1;
+
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+
+public class BreadthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public BreadthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        bfs(G, s);
+    }
+
+    public void bfs(Graph G, int s) {
+        Queue<Integer> queue = new Queue<>();
+        marked[s] = true;
+        queue.enqueue(s);
+        while (!queue.isEmpty()) {
+            int v = queue.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        Stack<Integer> path = new Stack<>();
+        for (int x = v; x != s ; x = edgeTo[x]) {
+            path.push(x);
+        }
+        path.push(s);
+        return path;
+    }
+}
