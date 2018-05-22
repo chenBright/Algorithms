6b0b0fdade1f9cf024c57eb3d49e5d11052dda02
chenBright <1021774709@qq.com>
2018-05-22 16:27:20 +0800

    Feature_4.1 习题4.1.23 (#111)

--- src/com/chenBright/algorithms/Main.java
+++ src/com/chenBright/algorithms/Main.java
@@ -24,7 +24,7 @@ public class Main {
     public static void main(String[] args) {
 
         StdOut.println("算法4");
-        SymbolGraph.main();
+        BaconHistogram.main();
 //        Ex1_4_19 ex = new Ex1_4_19();
 //        ex.main();
     }
