31db0a2a22e2916dc1d6c8c3fca11d3b1eae8f6c
chenBright <1021774709@qq.com>
2018-05-22 17:08:23 +0800

    Feature_4.2 Digraph (#113)

---
+++ src/com/chenBright/algorithms/chapter4_2/Digraph.java
@@ -0,0 +1,42 @@
+package com.chenBright.algorithms.chapter4_2;
+
+import edu.princeton.cs.algs4.Bag;
+
+// 无向图
+public class Digraph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Digraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        E++;
+    }
+
+    public Digraph reverse() {
+        Digraph R = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj[v]) {
+                R.addEdge(w, v);
+            }
+        }
+        return R;
+    }
+}
