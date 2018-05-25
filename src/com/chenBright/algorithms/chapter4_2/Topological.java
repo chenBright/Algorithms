80eac8d7084b1afbf9f86eb4f088cf69a70ec59e
chenBright <1021774709@qq.com>
2018-05-25 17:12:52 +0800

    Feature_4.1 Topological

--- src/com/chenBright/algorithms/chapter4_2/Topological.java
+++ src/com/chenBright/algorithms/chapter4_2/Topological.java
@@ -1,13 +1,35 @@
 package com.chenBright.algorithms.chapter4_2;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class Topological {
     private Iterable<Integer> order;
 
     public Topological(Digraph G) {
-        DirectedCycle cyclefinder = new DirectedCycle(G);
-        if (!cyclefinder.hasCycle()) {
-            
+        DirectedCycle cycleFinder = new DirectedCycle(G);
+        if (!cycleFinder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
         }
     }
 
+    public Iterable<Integer> order() {
+        return order;
+    }
+
+    public boolean isDAG() {
+        return order != null;
+    }
+
+    public static void main() {
+        String filename = "./data/jobs.txt";
+        String separator = "/";
+        SymbolDigraph sg = new SymbolDigraph(filename, separator);
+
+        Topological top = new Topological(sg.G());
+
+        for (int v : top.order()) {
+            StdOut.println(sg.name(v));
+        }
+    }
 }
