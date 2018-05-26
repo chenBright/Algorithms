ddad3dfad7e543c1d9647b7a6b7bafc85374ec8f
chenBright <1021774709@qq.com>
2018-05-26 15:21:18 +0800

    Feature_4.1 习题4.2.3 (#120)

--- src/com/chenBright/algorithms/chapter4_2/Digraph.java
+++ src/com/chenBright/algorithms/chapter4_2/Digraph.java
@@ -2,6 +2,7 @@ package com.chenBright.algorithms.chapter4_2;
 
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
 
 // 无向图
 public class Digraph {
@@ -25,6 +26,24 @@ public class Digraph {
             int v = in.readInt();
             int w = in.readInt();
             addEdge(v, w);
+        }
+    }
+
+    /**
+     * 习题 4.2.3
+     * @param G
+     */
+    public Digraph(Digraph G) {
+        this(G.V());
+        for (int v = 0; v < G.V(); v++) {
+            // 最后遍历到的顶点，先添加，才能保证副本跟G保持一致（遍历是，顶点的顺序）
+            Stack<Integer> stack = new Stack<>();
+            for (int w : G.adj(v)) {
+                stack.push(w);
+            }
+            for (int w : stack) {
+                addEdge(v, w);
+            }
         }
     }
 
