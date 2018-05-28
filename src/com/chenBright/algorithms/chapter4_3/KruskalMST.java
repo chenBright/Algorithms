90ef52243babff7101a15ef1d03bc4476e073c23
chenBright <1021774709@qq.com>
2018-05-28 17:09:42 +0800

    Feature_4.1 KruskalMST (#125)

---
+++ src/com/chenBright/algorithms/chapter4_3/KruskalMST.java
@@ -0,0 +1,33 @@
+package com.chenBright.algorithms.chapter4_3;
+
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.UF;
+
+public class KruskalMST {
+    private Queue<Edge> mst;
+
+    public KruskalMST(EdgeWeightedGraph G) {
+        mst = new Queue<>();
+        MinPQ<Edge> pq = new MinPQ<>();
+        for (Edge e : G.edges()) {
+            pq.insert(e);
+        }
+        UF uf = new UF(G.V());
+
+        while (!pq.isEmpty() && mst.size() < G.V() -  1) {
+            Edge e = pq.delMin();
+            int v = e.either();
+            int w = e.other(v);
+            if (uf.connected(v, w)) {
+                continue;
+            }
+            uf.union(v, w);
+            mst.enqueue(e);
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+}
