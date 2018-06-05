9d5f6593f44001a3e3c2fe3b7278dd610b239466
chenBright <1021774709@qq.com>
2018-06-05 15:34:41 +0800

    Feature_5.2 TrieST (#138)
    * Feature_4.4 TrieST
    
    * Feature_4.4 TrieST debug

---
+++ src/com/chenBright/algorithms/chapter5_2/TrieST.java
@@ -0,0 +1,160 @@
+package com.chenBright.algorithms.chapter5_2;
+
+import edu.princeton.cs.algs4.Queue;
+
+public class TrieST<Value> {
+    private static int R = 256; // 基数
+    private Node root;
+
+    private static class Node {
+        private Object val;
+        private Node[] next = new Node[R];
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        }
+        int count = 0;
+        if (x.val != null) {
+            count++;
+        }
+        for (int c = 0; c < R; c++) {
+            count += size(x.next[c]);
+        }
+        return count;
+    }
+
+    public Value get(String key) {
+        Node x = get(root, key, 0);
+        if (x.val != null) {
+            return null;
+        }
+        return (Value) x.val;
+    }
+
+    private Node get(Node x, String key, int d) {
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            return x;
+        }
+        char c = key.charAt(d);
+        return get(x.next[c], key, d + 1);
+    }
+
+    public void put(String key, Value val) {
+        root = put(root, key, val, 0);
+    }
+
+    private Node put(Node x, String key, Value val, int d) {
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            return x;
+        }
+        char c = key.charAt(d);
+        x.next[c] = put(x.next[c], key, val, d + 1);
+        return x;
+    }
+
+    public Iterable<String> keys() {
+        return keysWithPrefix("");
+    }
+
+    public Iterable<String> keysWithPrefix(String pre) {
+        Queue<String> q = new Queue<>();
+        collect(get(root, pre, 0), pre, q);
+        return q;
+    }
+
+    private void collect(Node x, String pre, Queue<String> q) {
+        if (x == null) {
+            return;
+        }
+        if (x.val != null) {
+            q.enqueue(pre);
+        }
+        for (int c = 0; c < R; c++) {
+            collect(x.next[c], pre + c, q);
+        }
+    }
+
+    public Iterable<String> keysThatMatch(String pat) {
+        Queue<String> q = new Queue<>();
+        collect(root, "", pat, q);
+        return q;
+    }
+
+    private void collect(Node x, String pre, String pat, Queue<String> q) {
+        int d = pre.length();
+        if (x == null) {
+            return;
+        }
+        if (d == pat.length() && x.val != null) {
+            q.enqueue(pre);
+        }
+        if (d == pat.length()) {
+            return;
+        }
+        char next = pat.charAt(d);
+        for (char c = 0; c < R; c++) {
+            if (next == '.' || next == c) {
+                collect(x.next[c], pre + c, pat, q);
+            }
+        }
+    }
+
+    public String longestPrefixOf(String s) {
+        int length = search(root, s, 0, 0);
+        return s.substring(0, length);
+    }
+
+    private int search(Node x, String s, int d ,int length) {
+        if (x == null) {
+            return length;
+        }
+        if (x.val != null) {
+            length = d;
+        }
+        if (d == s.length()) {
+            return length;
+        }
+        char c = s.charAt(d);
+        return search(x.next[c], s, d + 1, length);
+    }
+
+    public void delete(String key) {
+        root = delete(root, key, 0);
+    }
+
+    private Node delete(Node x, String key, int d) {
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            x.val = null;
+        }
+        else {
+            char c = key.charAt(d);
+            x.next[c] = delete(x.next[c], key, d + 1);
+        }
+
+        if (x.val != null) {
+            return x;
+        }
+        for (char c = 0; c < R; c++) {
+            if (x.next[c] != null) {
+                return x;
+            }
+        }
+        // 该节点的所有链接均为空
+        return null;
+    }
+}
