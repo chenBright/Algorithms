21fb2831e2cc4754b1ab8884a428d8d5e828d457
chenBright <1021774709@qq.com>
2018-06-07 19:23:11 +0800

    Feature_5.3 KMP (#141)

---
+++ src/com/chenBright/algorithms/chapter5_2/KMP.java
@@ -0,0 +1,49 @@
+package com.chenBright.algorithms.chapter5_2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class KMP {
+    private String pat;
+    private int[][] dfa;
+
+    public KMP(String pat) {
+        this.pat = pat;
+        int M = pat.length();
+        int R = 256;
+        dfa = new int[R][M];
+        dfa[pat.charAt(0)][0] = 1;
+        for (int X = 0, j = 1; j < M; j++) {
+            for (int c = 0; c < R; c++) {
+                dfa[c][j] = dfa[c][X];
+            }
+            dfa[pat.charAt(j)][j] = j + 1;
+            X = dfa[pat.charAt(j)][X];
+        }
+    }
+
+    public int search(String txt) {
+        int i, j, N = txt.length(), M = pat.length();
+        for (i = 0, j = 0; i < N && j < M; i++) {
+            j = dfa[txt.charAt(i)][j];
+        }
+        if (j == M) {
+            return i - M;
+        }
+        else {
+            return N;
+        }
+    }
+
+    public static void main() {
+        String pat = "ABABAC";
+        String txt = "BCBAABACAABABACAA";
+        KMP kmp = new KMP(pat);
+        StdOut.println("text:    " + txt);
+        int offset = kmp.search(txt);
+        StdOut.print("pattern: ");
+        for (int i = 0; i < offset; i++) {
+            StdOut.print(" ");
+        }
+        StdOut.println(pat);
+    }
+}
