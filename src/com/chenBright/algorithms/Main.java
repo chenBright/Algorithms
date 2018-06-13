0d55dad3c5f97fa724a9d0189091b6467b630d00
chenBright <1021774709@qq.com>
2018-06-13 15:50:19 +0800

    Feature5.3 BoyerMoore

--- src/com/chenBright/algorithms/Main.java
+++ src/com/chenBright/algorithms/Main.java
@@ -22,13 +22,14 @@ import com.chenBright.algorithms.chapter4_3.*;
 import com.chenBright.algorithms.chapter4_4.*;
 import com.chenBright.algorithms.chapter5_1.*;
 import com.chenBright.algorithms.chapter5_2.*;
+import com.chenBright.algorithms.chapter5_3.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         StdOut.println("算法4");
-        KMP.main();
+        BoyerMoore.main();
 //        Ex1_4_19 ex = new Ex1_4_19();
 //        ex.main();
     }
